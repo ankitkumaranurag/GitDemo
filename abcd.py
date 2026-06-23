4f860808be6a24296863373dc32e498775b970c3
Ankit Kumar <ak1402210@gmail.com>
2026-06-23 20:16:00 +0530

    Hello, Ankit!

---
+++ abcd.py
@@ -0,0 +1,36 @@
+[33mcommit d518e8044925b7f734efa733455793bc7501f949[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Merge: 098cf81 e24e840
+Author: Ankit Kumar <ak1402210@gmail.com>
+Date:   Tue Jun 23 20:06:48 2026 +0530
+
+    Merge remote and local repositories
+
+[33mcommit 098cf81e70c72fb2d10d8c9310c21ef9db98fd21[m
+Author: Ankit Kumar <ak1402210@gmail.com>
+Date:   Tue Jun 23 19:53:49 2026 +0530
+
+    Add abc.py with print statement for Hello Ankit
+
+[33mcommit e24e8405a0271719344635186d9d84d33a26649e[m
+Author: Ankit Kumar <ak1402210@gmail.com>
+Date:   Tue Jun 16 22:26:43 2026 +0530
+
+    Update with hello and ankit prints
+
+[33mcommit a19dcdcfc6be640267cfd3f092697006bce48102[m
+Author: Ankit Kumar <ak1402210@gmail.com>
+Date:   Tue Jun 16 22:12:52 2026 +0530
+
+    Add hello and ankit prints
+
+[33mcommit b3c52ea0625dd24f35dfd372e5a469772b1a0d67[m
+Author: ankitkumaranurag <ak1402210@gmail.com>
+Date:   Tue Jun 16 21:36:06 2026 +0530
+
+    Add abc.py
+
+[33mcommit 71bbe8f3135ed3f28e52cf0838430e85e3339917[m
+Author: Ankit Kumar <ak1402210@gmail.com>
+Date:   Tue Jun 16 20:31:53 2026 +0530
+
+    Python_Mini_Project
